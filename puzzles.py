ac9d4a7f8d1f2e88b40d9cef32b9c47840a2e7f6
“yuanahao” <youthandqueen@qq.com>
2025-08-28 21:57:14 +0800

    feat: finish the puzzle 3, Outer Vector Add.

--- puzzles.py
+++ puzzles.py
@@ -271,7 +271,15 @@ def add_vec_spec(x: Float32[32,], y: Float32[32,]) -> Float32[32, 32]:
 
 @triton.jit
 def add_vec_kernel(x_ptr, y_ptr, z_ptr, N0, N1, B0: tl.constexpr, B1: tl.constexpr):
-    # Finish me!
+    off_x = tl.arange(0, B0)
+    off_y = tl.arange(0, B1)
+    x = tl.load(x_ptr + off_x)
+    y = tl.load(y_ptr + off_y)
+    z = add_vec_spec(x, y)
+    off_i = tl.arange(0, B0)[None, :]  # cols
+    off_j = tl.arange(0, B1)[:, None]  # rows
+    offset = off_j * N0 + off_i
+    tl.store(z_ptr + offset, z)
     return
 
 
